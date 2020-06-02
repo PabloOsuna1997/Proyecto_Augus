951ec092f8013830a2f96f24260fc5a15e2e4a5e
PabloOsuna1997 <2472932372001@ingenieria.usac.edu.gt>
2020-06-02 00:27:02 -0600

    feat: New features

--- src/gui.py
+++ src/gui.py
@@ -6,23 +6,28 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-
+import sys 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
+#from PyQt5.QtGui import QColor
 
 contadorVentanas = 0
+
 class Ui_Augus(object):
     def setupUi(self, Augus):
         Augus.setObjectName("Augus")
-        Augus.resize(877, 757)
+        Augus.resize(898, 616)
         self.centralwidget = QtWidgets.QWidget(Augus)
         self.centralwidget.setObjectName("centralwidget")
         self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
         self.tabWidget.setGeometry(QtCore.QRect(20, 20, 471, 541))
         self.tabWidget.setObjectName("tabWidget")
+        self.textEditConsole = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEditConsole.setGeometry(QtCore.QRect(510, 20, 361, 541))
+        self.textEditConsole.setObjectName("textEditConsole")
         Augus.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(Augus)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 877, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 898, 21))
         self.menubar.setObjectName("menubar")
         self.menuArchivo = QtWidgets.QMenu(self.menubar)
         self.menuArchivo.setObjectName("menuArchivo")
@@ -48,6 +53,8 @@ class Ui_Augus(object):
         self.actionGuardar_Como.setObjectName("actionGuardar_Como")
         self.actionCerrar = QtWidgets.QAction(Augus)
         self.actionCerrar.setObjectName("actionCerrar")
+        self.actionSalir = QtWidgets.QAction(Augus)
+        self.actionSalir.setObjectName("actionSalir")
         self.actionCopiar = QtWidgets.QAction(Augus)
         self.actionCopiar.setObjectName("actionCopiar")
         self.actionPegar = QtWidgets.QAction(Augus)
@@ -58,6 +65,8 @@ class Ui_Augus(object):
         self.actionBuscar.setObjectName("actionBuscar")
         self.actionAscendente = QtWidgets.QAction(Augus)
         self.actionAscendente.setObjectName("actionAscendente")
+        self.actionDescendente = QtWidgets.QAction(Augus)
+        self.actionDescendente.setObjectName("actionDescendente")
         self.actionCambiar_color_de_fondo = QtWidgets.QAction(Augus)
         self.actionCambiar_color_de_fondo.setObjectName("actionCambiar_color_de_fondo")
         self.actionAyuda = QtWidgets.QAction(Augus)
@@ -67,11 +76,13 @@ class Ui_Augus(object):
         self.menuArchivo.addAction(self.actionGuardar)
         self.menuArchivo.addAction(self.actionGuardar_Como)
         self.menuArchivo.addAction(self.actionCerrar)
+        self.menuArchivo.addAction(self.actionSalir)
         self.menuEditar.addAction(self.actionCopiar)
         self.menuEditar.addAction(self.actionPegar)
         self.menuEditar.addAction(self.actionCortar)
         self.menuEditar.addAction(self.actionBuscar)
         self.menuEjecutar.addAction(self.actionAscendente)
+        self.menuEjecutar.addAction(self.actionDescendente)
         self.menuOpciones.addAction(self.actionCambiar_color_de_fondo)
         self.menuAyuda.addAction(self.actionAyuda)
         self.menubar.addAction(self.menuArchivo.menuAction())
@@ -84,13 +95,16 @@ class Ui_Augus(object):
         self.tabWidget.setCurrentIndex(-1)
         QtCore.QMetaObject.connectSlotsByName(Augus)
 
-
+        
         #actions
         self.actionNuevo.triggered.connect(lambda : self.fn_Nuevo())
         self.actionAbrir.triggered.connect(lambda : self.fn_Abrir())
-        self.actionAscendente.triggered.connect(lambda : self.fn_Ejecutar())
+        self.actionAscendente.triggered.connect(lambda : self.fn_Ejecutar_Ascendente())
+        self.actionDescendente.triggered.connect(lambda : self.fn_Ejecutar_Descendente())
         self.actionGuardar.triggered.connect(lambda : self.fn_Guardar())
         self.actionGuardar_Como.triggered.connect(lambda : self.fn_Guardar_Como())
+        self.actionCerrar.triggered.connect(lambda : self.fn_Cerrar())
+        self.actionSalir.triggered.connect(lambda : self.fn_Salir())
 
     def retranslateUi(self, Augus):
         _translate = QtCore.QCoreApplication.translate
@@ -105,11 +119,13 @@ class Ui_Augus(object):
         self.actionGuardar.setText(_translate("Augus", "Guardar"))
         self.actionGuardar_Como.setText(_translate("Augus", "Guardar Como"))
         self.actionCerrar.setText(_translate("Augus", "Cerrar"))
+        self.actionSalir.setText(_translate("Augus", "Salir"))
         self.actionCopiar.setText(_translate("Augus", "Copiar"))
         self.actionPegar.setText(_translate("Augus", "Pegar"))
         self.actionCortar.setText(_translate("Augus", "Cortar"))
         self.actionBuscar.setText(_translate("Augus", "Buscar"))
         self.actionAscendente.setText(_translate("Augus", "Ascendente"))
+        self.actionDescendente.setText(_translate("Augus","Descendente"))
         self.actionCambiar_color_de_fondo.setText(_translate("Augus", "Cambiar color de fondo"))
         self.actionAyuda.setText(_translate("Augus", "Ayuda"))
 
@@ -126,31 +142,53 @@ class Ui_Augus(object):
         )
         self.tabWidget.setCurrentIndex(self.tabWidget.count()-1)    #para poner el focus en la nueva pestaña
 
+        self.msgBox = QtWidgets.QMessageBox()
+        self.msgBox.setText("New area.")
+        self.msgBox.exec()
+
     def fn_Abrir(self):
-        filename = QFileDialog.getOpenFileName(None,' Open document',r"c:\\Users\\", "All Files (*)")
-        path = filename[0]
-        print(path)
-        with open(path, 'r') as f:
-            data = f.read()
-           
-            #creating a new tab
-            self.tab = QtWidgets.QWidget()
-            self.tab.setObjectName("tab")
-            self.textEdit = QtWidgets.QTextEdit(self.tab)
-            self.textEdit.setGeometry(QtCore.QRect(0,0,440,500))
-            self.textEdit.setObjectName("textEdit")
-            self.textEdit.setPlainText(data)
-            self.tabWidget.addTab(
-                self.tab,path
-            )
-            self.tabWidget.setCurrentIndex(self.tabWidget.count()-1)    #para poner el focus en la nueva pestaña
+        try:
+            filename = QFileDialog.getOpenFileName(None,' Open document',r"c:\\Users\\", "All Files (*)")
+            path = filename[0]
+            print(path)
+            with open(path, 'r') as f:
+                data = f.read()
+            
+                #creating a new tab
+                self.tab = QtWidgets.QWidget()
+                self.tab.setObjectName("tab")
+                self.textEdit = QtWidgets.QTextEdit(self.tab)
+                self.textEdit.setGeometry(QtCore.QRect(0,0,440,500))
+                self.textEdit.setObjectName("textEdit")
+                self.textEdit.setPlainText(data)
+                #self.textEdit.setTextBackgroundColor( QColor ( "Gray"))
+                self.tabWidget.addTab(
+                    self.tab,path
+                )
+                self.tabWidget.setCurrentIndex(self.tabWidget.count()-1)    #para poner el focus en la nueva pestaña
 
-            #close the file
-            f.close()
+                #close the file
+                f.close()
+        except:
+            print("closing file dialog.")
 
-    def fn_Ejecutar(self):
-        content = self.tabWidget.currentWidget().findChild(QtWidgets.QTextEdit,"textEdit").toPlainText()
-        print("contenido a ejecutar: " + content)
+    def fn_Ejecutar_Ascendente(self):
+        try:
+            content = self.tabWidget.currentWidget().findChild(QtWidgets.QTextEdit,"textEdit").toPlainText()
+            print("contenido a ejecutar de manera ascendente: " + content)        
+        except:
+            self.msgBox = QtWidgets.QMessageBox()
+            self.msgBox.setText("Empty Area.")
+            self.msgBox.exec()
+    
+    def fn_Ejecutar_Descendente(self):
+        try:
+            content = self.tabWidget.currentWidget().findChild(QtWidgets.QTextEdit,"textEdit").toPlainText()
+            print("contenido a ejecutar de manera descendente: " + content)        
+        except:
+            self.msgBox = QtWidgets.QMessageBox()
+            self.msgBox.setText("Empty Area.")
+            self.msgBox.exec()
 
     def fn_Guardar(self):
         #content to save
@@ -163,18 +201,32 @@ class Ui_Augus(object):
         f.close()
     
     def fn_Guardar_Como(self):
-        content = self.tabWidget.currentWidget().findChild(QtWidgets.QTextEdit,"textEdit").toPlainText()
-        filename = QFileDialog.getSaveFileName(None)
-        path = filename[0]
-        
-        print("writing to file.")
-        f=open(path,"w+")
-        f.write(content)
-        f.close()
+        try:
+            content = self.tabWidget.currentWidget().findChild(QtWidgets.QTextEdit,"textEdit").toPlainText()
+            filename = QFileDialog.getSaveFileName(None)
+            path = filename[0]
+            
+            print("writing to file.")
+            f=open(path,"w+")
+            f.write(content)
+            f.close()
 
-        #updating title on the tab
-        self.tabWidget.setTabText(self.tabWidget.currentIndex(), path)
+            #updating title on the tab
+            self.tabWidget.setTabText(self.tabWidget.currentIndex(), path)
+        except:
+            print("closing file dialog")
 
+    def fn_Cerrar(self):
+        self.tabWidget.removeTab(self.tabWidget.currentIndex())
+        self.msg = QtWidgets.QMessageBox()
+        self.msg.setText("Closed Table.")
+        self.msg.exec()
+
+    def fn_Salir(self):
+        self.msg = QtWidgets.QMessageBox()
+        self.msg.setText("¡Good Bye!")
+        self.msg.exec()
+        sys.exit(1)
 
 if __name__ == "__main__":
     import sys
